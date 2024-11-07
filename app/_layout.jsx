275d2cea106e4e732dbeec53639dfc89e54d9cba
Muhammad Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-11-07 18:00:12 +0000

    Set up Tailwind CSS with NativeWind and remove default App component

---
+++ app/_layout.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+import './global.css'
+const _layout = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>_layout</Text>
+    </View>
+  )
+}
+
+export default _layout
+
